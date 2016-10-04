d22debbcfa869729cba9ecae5aea66860f3e3864
Murat1993 <muratbek199325@gmail.com>
2016-10-04 09:47:18 +0600

    Init

--- src/java/net/murat/springsecurityapp/dao/RoleDao.java
+++
@@ -1,8 +0,0 @@
-package net.murat.springsecurityapp.dao;
-
-
-import net.murat.springsecurityapp.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleDao extends JpaRepository<Role, Long> {
-}
--- src/java/net/murat/springsecurityapp/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package net.murat.springsecurityapp.dao;
-
-
-import net.murat.springsecurityapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
